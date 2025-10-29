a46f9da521d4656323debe4d5b62e119a84b040c
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2025-10-29 23:37:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3525319952-f1fd9",
+  "appId": "1:775649749319:web:47425ae819e17a962e4462",
+  "apiKey": "AIzaSyCYIizn5J4qls7sNsjjqzx9KXCGmzsjXYs",
+  "authDomain": "studio-3525319952-f1fd9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "775649749319"
+};
