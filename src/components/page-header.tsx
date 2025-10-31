d731f6eb1906674bcbc0730096b1680c4c4f0382
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2025-10-31 21:30:34 +0000

    Mude o título para vermelho (_for element h1_)

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -9,7 +9,7 @@ type PageHeaderProps = {
 export default function PageHeader({ title, description, className }: PageHeaderProps) {
   return (
     <div className={cn("grid gap-1", className)}>
-        <h1 className="text-3xl md:text-4xl font-bold tracking-tight text-foreground font-headline">
+        <h1 className="text-3xl md:text-4xl font-bold tracking-tight text-primary font-headline">
             {title}
         </h1>
         {description && (
