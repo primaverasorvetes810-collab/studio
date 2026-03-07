698501ec01379f31ef02eaaaba2d0aaf0c257513
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2026-03-07 22:40:58 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3525319952-f1fd9",
   "appId": "1:775649749319:web:47425ae819e17a962e4462",
+  "storageBucket": "studio-3525319952-f1fd9.appspot.com",
   "apiKey": "AIzaSyCYIizn5J4qls7sNsjjqzx9KXCGmzsjXYs",
   "authDomain": "studio-3525319952-f1fd9.firebaseapp.com",
   "measurementId": "",
