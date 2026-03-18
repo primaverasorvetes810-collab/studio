6aa9d47e7df665fad43b8b6f6a29d68b45524315
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2026-03-18 23:08:33 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,14 +1,15 @@
-
 import Image from 'next/image';
 import type { HTMLAttributes } from "react";
+import { cn } from '@/lib/utils';
 
 export function PrimaveraLogo(props: HTMLAttributes<HTMLDivElement>) {
   return (
-    <div {...props} className={`relative w-32 ${props.className || ''}`}>
+    <div {...props} className={cn("relative", props.className)}>
       <Image
         src="https://res.cloudinary.com/dh88bfqo0/image/upload/v1773874955/logo_primavera_delivery_jcnww8.png"
         alt="Primavera Delivery Logo"
-        fill
+        width={111}
+        height={32}
         priority
         className="object-contain"
       />
