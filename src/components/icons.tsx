52480bdae9d5d0092be193ac0048f93b82fa6712
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2026-03-18 23:20:41 +0000

    https://res.cloudinary.com/dh88bfqo0/image/upload/v1773874955/logo_prima

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 
 export function PrimaveraLogo(props: HTMLAttributes<HTMLDivElement>) {
   return (
-    <div {...props} className={cn("relative w-[111px]", props.className)}>
+    <div {...props} className={cn("relative", props.className)}>
       <Image
         src="https://res.cloudinary.com/dh88bfqo0/image/upload/v1773874955/logo_primavera_delivery_jcnww8.png"
         alt="Primavera Delivery Logo"
