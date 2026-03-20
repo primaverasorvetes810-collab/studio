a994052d14aacdee8c44c481ff3092e6a21e4c27
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2026-03-20 21:01:39 +0000

    Make the following changes:
    Find this element:
    
      <LinkComponent href="

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,13 +9,13 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "border-2 bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "border bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "border-2 bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border-2 border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "border bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "border-2 bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
