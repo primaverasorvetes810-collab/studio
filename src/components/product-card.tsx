66709a9d6c4c68da30ee7263f7e8adedb2585d6b
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2026-03-20 21:28:45 +0000

    Aumente mais o texto do valor e deixe os botões e os preços em um tom sa

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -84,7 +84,7 @@ export function ProductCard({ product }: ProductCardProps) {
           <CardDescription className="line-clamp-2 text-xs">{product.description}</CardDescription>
         </div>
         <div className="mt-2">
-            <p className="text-4xl font-bold text-primary">{isMounted ? formatPrice(product.price) : formatPriceAsString(product.price)}</p>
+            <p className="text-5xl font-bold text-primary">{isMounted ? formatPrice(product.price) : formatPriceAsString(product.price)}</p>
         </div>
       </CardContent>
       <CardFooter className="p-2 pt-0">
