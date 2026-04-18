19967cc1ab93ffce8bb01a42ac5140ba6412c95d
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2026-04-18 00:34:51 +0000

    Digite seu E-mail (_for element <Primitive.label>_)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -123,7 +123,7 @@ export default function LoginPage() {
               name="email"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>E-mail</FormLabel>
+                  <FormLabel>Digite seu E-mail</FormLabel>
                   <FormControl>
                     <Input placeholder="m@example.com" {...field} />
                   </FormControl>
