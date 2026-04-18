496026a69acedfc748e246af8deb61972145b573
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2026-04-18 18:46:42 +0000

    Deixe com -4 de tamanho  (_for element span_)

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -153,7 +153,7 @@ export default function OrdersPage() {
                 <AccordionTrigger className="px-6 py-8 hover:no-underline">
                   <div className="flex w-full items-center justify-between">
                     <div className="flex flex-col items-start text-left">
-                      <span className="font-bold text-2xl">{statusDisplayMessages[order.status]}</span>
+                      <span className="font-bold text-lg">{statusDisplayMessages[order.status]}</span>
                     </div>
                     <div className="flex items-center gap-4">
                        <span className="hidden sm:inline font-bold text-primary">{isMounted ? formatPrice(order.totalAmount) : formatPriceAsString(order.totalAmount)}</span>
