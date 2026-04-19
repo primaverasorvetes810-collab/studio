78b16c0f3f7bca1336dc939458a67ad1468433a6
Primavera Sorvetes <primaverasorvetes810@gmail.com>
2026-04-19 00:16:28 +0000

    De um poouco MUITO POUCO respiro para esse dialogo. (_for element <Input

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -94,12 +94,12 @@ export default function ProductsPage() {
   return (
     <div className="pb-32">
       <HomeCarousel />
-      <div className="relative">
+      <div className="relative py-2">
         <Search className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground z-10" />
         <Input
           type="search"
           placeholder="O que você procura?"
-          className="w-full pl-12 h-8 text-base rounded-none border-x-0 border-t-0"
+          className="w-full pl-12 h-8 text-base rounded-none border-x-0"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
